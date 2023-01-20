42a510c213b694ad6c1cb3f8001ab0c9c275e83b
replayzor <oltean.ionut1989@gmail.com>
2023-01-20 12:41:36 +0200

    added title state, handleChange, handleSubmit and the form

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,5 +1,27 @@
-function BookCreate() {
-	return <div>BookCreate</div>;
+import { useState } from "react";
+
+function BookCreate({ handleCreateBook }) {
+	const [title, setTitle] = useState("");
+
+	const handleChange = (e) => {
+		setTitle(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		handleCreateBook(title);
+		setTitle("");
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>Title</label>
+				<input value={title} onChange={handleChange} />
+				<button>Add Book</button>
+			</form>
+		</div>
+	);
 }
 
 export default BookCreate;
