3ddf2dcb49b8249ff902d5a9ce5c518fa9d62b9f
replayzor <oltean.ionut1989@gmail.com>
2023-01-20 17:52:07 +0200

    added updatedBooks to handleCreateBook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+// components
 import BookCreate from "./components/BookCreate";
 
 function App() {
 	const [books, setBooks] = useState([]);
 
 	const handleCreateBook = (title) => {
-		console.log("The title is:", title);
+		const updatedBooks = [...books, { id: uuidv4(), title }];
+		setBooks(updatedBooks);
 	};
 
 	return (
 		<div>
+			Total books of: {books.length}
 			<BookCreate handleCreateBook={handleCreateBook} />
 		</div>
 	);
