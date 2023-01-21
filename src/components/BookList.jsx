86a5e70da95ced670e48b4652db1aa6b59532089
replayzor <oltean.ionut1989@gmail.com>
2023-01-21 11:02:47 +0200

    map over the books component pass it to BookShow and siplay renderedBooks

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,12 @@
-function BookList() {
-	return <div>BookList</div>;
+import BookShow from "./BookShow";
+
+function BookList({ books }) {
+	const renderedBooks = books.map((book) => {
+		const { id } = book;
+		return <BookShow key={id} book={book} />;
+	});
+
+	return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
